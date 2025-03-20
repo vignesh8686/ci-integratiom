61aade9c25412350e9b42cb4f9d53e044d4aeea1
vignesh8686 <vigneshdonthi@gmail.com>
2025-03-20 14:04:48 +0530

    cli-2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.write("enter a number to calculate its square,cube,and fifth power.")
 n=st.number_input("Enter a integer",value=1,step=1)
 
 square=n**2
-cube=n**3
+cube=n**5
 fifth_power=n**5
 
 st.write(f"the square of {n} is :{square}")
